bb5fe75be2c064e8e5c1e5bf447f754dbd02e885
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-04-18 17:04:56 +0530

    feat: add loading indicator

--- lib/image_screening/image_screen.dart
+++ lib/image_screening/image_screen.dart
@@ -16,6 +16,7 @@ class ImageScreen extends StatefulWidget {
 class _ImageScreenState extends State<ImageScreen> {
   File? _image;
   late ImageScreeningProvider provider;
+  bool isLoading = false;
   @override
   void initState() {
     super.initState();
@@ -29,52 +30,60 @@ class _ImageScreenState extends State<ImageScreen> {
         centerTitle: true,
         title: const Text('Image Screening'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            iconSize: 100,
-            onPressed: () async {
-              final pickedFile =
-                  await ImagePicker().pickImage(source: ImageSource.gallery);
-              if (pickedFile != null) {
-                setState(() {
-                  _image = File(pickedFile.path);
-                });
-              }
-            },
-            icon: const Icon(
-              Icons.image,
-            ),
-          ),
-          const SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: ElevatedButton(
-              onPressed: _image != null
-                  ? () async {
-                      if (_image != null) {
-                        bool isNsfw = await provider.verifyImage(_image!);
-                        if (isNsfw) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            _snackBar("Image is Inappropriate",
-                                color: Colors.red),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            _snackBar("Image is Appropriate",
-                                color: Colors.green),
-                          );
-                        }
-                      }
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  iconSize: 100,
+                  onPressed: () async {
+                    final pickedFile = await ImagePicker()
+                        .pickImage(source: ImageSource.gallery);
+                    if (pickedFile != null) {
+                      setState(() {
+                        _image = File(pickedFile.path);
+                      });
                     }
-                  : null,
-              child: const Text('Verify'),
+                  },
+                  icon: const Icon(
+                    Icons.image,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: _image != null
+                        ? () async {
+                            if (_image != null) {
+                              bool isNsfw = await provider.verifyImage(_image!);
+
+                              setState(() {
+                                isLoading = true;
+                              });
+                              if (isNsfw) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  _snackBar("Image is Inappropriate",
+                                      color: Colors.red),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  _snackBar("Image is Appropriate",
+                                      color: Colors.green),
+                                );
+                              }
+                            }
+                          }
+                        : null,
+                    child: const Text('Verify'),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 
