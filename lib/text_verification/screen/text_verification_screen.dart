cd0564e56f3807f2007d40b33edb134c5710364c
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-04-19 16:48:53 +0530

    ref: renamed text screen

--- lib/text_verification/screen/text_verification_screen.dart
+++ lib/text_verification/screen/text_verification_screen.dart
@@ -1,7 +1,6 @@
 import 'package:contentscanner/text_verification/bloc/text_verification_screen_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:profanity_filter/profanity_filter.dart';
 
 class TextScreen extends StatefulWidget {
   const TextScreen({super.key});
